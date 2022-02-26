412945d557b3ce26513e1e481f7b1cf1ef022ffb
gg22hh <g2203h@gmail.com>
2022-02-26 20:07:58 +0300

    finished banner

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import'./Head.css'
 import { Header } from './HeadComponents/Header/Header';
+import { Banner } from './HeadComponents/Banner/Banner';
 
 export const Head = () => {
 	return (
         <div className="head">
             <div className="container">
 				<Header />
+				<Banner />
             </div>
         </div>
     );
---
+++ src/components/Head/HeadComponents/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Banner.css'
+
+export const Banner = () => {
+	return (
+        <div className="banner">
+            <h1 className="banner__title">
+                Railtrips To Here, There And Everywhere!
+            </h1>
+            <div className="banner__text">
+                We all wish to start our year the best way possible and also
+                according to a common belief if you have a great start to your.
+            </div>
+            <a className="banner__button" href="#">
+                Explore more
+            </a>
+        </div>
+    );
+}
