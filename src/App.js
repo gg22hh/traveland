a75e28960c9516065715c9393ac9740b077d03b0
gg22hh <g2203h@gmail.com>
2022-02-27 16:18:39 +0300

    started offers section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Head } from './components/Head/Head'
 import { Info } from './components/Info/Info';
+import { Offers } from './components/Offers/Offers';
 
 function App() {
   return (
     <div>
 	  <Head />
 	  <Info />
+	  <Offers />
     </div>
   );
 }
---
+++ src/components/Offers/Offers.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Offers.css'
+
+export const Offers = () => {
+	return (
+        <div className="offers">
+            <div className="container">
+                <h2 className="offers__title title">Special Offers</h2>
+                <div className="offers__text">
+                    With the New Year comes a refreshing sense of wanderlust, a
+                    longing in our souls to escape to warmer climates,
+                </div>
+            </div>
+        </div>
+    );
+}
